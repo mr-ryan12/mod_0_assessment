b048595f3f4593387033c549338412f95cb3954b
Ryan McBride <mr.ryan444@gmail.com>
2021-07-30 20:20:55 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var animal = 'Dog';
+var isCuddly = true;
+animal = 'Elephant';
+
+var num8 = 8;
+var num3 = 3;
+var quotient = num3 / num8;
+
+var favoriteFilms = ['Boondock Saints', 'The Departed', 'Reservoir Dogs'];
+favoriteFilms.shift();
+
+console.log(animal.length);
