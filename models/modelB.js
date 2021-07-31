54c907f1e50076506c476683bad7ff46a121a566
Ryan McBride <mr.ryan444@gmail.com>
2021-07-30 20:40:42 -0600

    Add house object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+    address: '1234 Main Ave',
+    city: 'Denver',
+    numOfWindows: 7,
+    numOfDoors: 1,
+    isHotInside: true,
+    needsCleaning: true,
+    pantryContents: ['dark chocolate bar', 'spices', 'cookies'],
+    fridgeContents: ['eggs', 'grapefruit', 'lettuce']
+}
