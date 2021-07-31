b0126e53bad35fed0f91de1620297b8e3cc4b950
Ryan McBride <mr.ryan444@gmail.com>
2021-07-30 20:33:55 -0600

    Add laptop object

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var laptop = {
+    manufacturer: 'Apple',
+    model: 'MacBook Pro',
+    year: 2020,
+    numOfPorts: 2,
+    isTurnedOn: true,
+    isConnectedToWifi: true,
+    applications: ['Google Chrome', 'VS Code', 'Atom'],
+    hardware: ['internal keyboard', 'external monitor', 'touchpad']
+};
+
